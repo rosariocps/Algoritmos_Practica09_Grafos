bf6913dc06d755e38007752a857eafdac5f72141
Janitza Tapia <janitzat4@gmail.com>
2025-06-05 21:56:03 -0500

    actividad 2-Edge

--- graph/Edge.java
+++ graph/Edge.java
@@ -1,5 +1,34 @@
 package graph;
 
-public class Edge {
-    
+public class Edge<E> {
+    private Vertex<E> refDest;
+    private int weight;
+
+    public Edge(Vertex<E> refDest) {
+        this(refDest, -1);
+    }
+
+    public Edge(Vertex<E> refDest, int weight) {
+        this.refDest = refDest;
+        this.weight = weight;
+    }
+
+    public Vertex<E> getRefDest() {
+        return refDest;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Edge<?>) {
+            Edge<E> e = (Edge<E>) o;
+            return this.refDest.equals(e.refDest);
+        }
+        return false;
+    }
+
+    public String toString() {
+        if (this.weight > -1)
+            return refDest.getData() + " [" + this.weight + "], ";
+        else
+            return refDest.getData() + ", ";
+    }
 }
