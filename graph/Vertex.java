7a351d1260544941ce921214869469bb041cf3e8
janitzat4@gmail.com <janitzat4@gmail.com>
2025-06-06 09:24:37 -0500

    comentarios en Vertex

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -4,7 +4,8 @@ import linkedlist.ListaEnlazada;
 
 public class Vertex<E> {
     private E data;
-    protected ListaEnlazada<Edge<E>> listAdj;
+    protected ListaEnlazada<Edge<E>> listAdj; // lista de adyacencia
+    // para almacenar las aristas que conectan este vértice con otros
 
     public Vertex(E data) {
         this.data = data;
@@ -14,13 +15,17 @@ public class Vertex<E> {
     public E getData() {
         return data;
     }
-
-    public boolean equals(Object o) {
-        if (o instanceof Vertex<?>) {
-            Vertex<E> v = (Vertex<E>) o;
-            return this.data.equals(v.data);
+    //para comparar vértices por su dato
+    public boolean equals(Object o) { //parametro de tipo Object para poder comparar cualquier objeto)
+        //si el objeto o es una instancia de Vertex
+        //se usa <?> porque el tipo generico (<E>) puede ser cualquiera (String, Integer, etc.)
+        if (o instanceof Vertex<?>) { 
+            Vertex<E> v = (Vertex<E>) o; //hacemos un cast de o al tipo Vertex<E>
+            //compara el valor almacenado en el vertice actual (this.data) con el valor del vértice recibido como argumento (v.data)
+            return this.data.equals(v.data); 
+            //si son iguales devuelve true si no devuelve false
         }
-        return false;
+        return false; //si el objeto o no era un vertice se devuelve false
     }
 
     public String toString() {
