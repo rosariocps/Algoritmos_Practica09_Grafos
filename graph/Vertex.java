c7016c01f819c841d612325c0dbb003900b406bf
Janitza Tapia <janitzat4@gmail.com>
2025-06-05 21:54:28 -0500

    actividad 2-Vertex

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -1,5 +1,29 @@
 package graph;
 
-public class Vertex {
-    
+import linkedlist.ListaEnlazada;
+
+public class Vertex<E> {
+    private E data;
+    protected ListaEnlazada<Edge<E>> listAdj;
+
+    public Vertex(E data) {
+        this.data = data;
+        listAdj = new ListaEnlazada<Edge<E>>();
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Vertex<?>) {
+            Vertex<E> v = (Vertex<E>) o;
+            return this.data.equals(v.data);
+        }
+        return false;
+    }
+
+    public String toString() {
+        return this.data + " --> " + this.listAdj.toString() + "\n";
+    }
 }
